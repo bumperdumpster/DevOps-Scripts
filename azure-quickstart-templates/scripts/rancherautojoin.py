6cb563574d9d7fcfa7122dcba9355ad10c0f6d38
Dmitry Tokarev <dm.tokarev@yahoo.com>
2017-08-15 11:12:32 -0700

    Performance and code style improvements for rancherautojoin.py

--- azure-quickstart-templates/scripts/rancherautojoin.py
+++ azure-quickstart-templates/scripts/rancherautojoin.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
+import getopt
 import os
 import sys
-import getopt
-import requests, json
 from time import sleep
 
+import requests
+
+
 # usage help message
-def usage(msg = None):
-        if not msg == None:
+def usage(msg=None):
+        if msg is not None:
                 print msg
                 print
         print "Usage: new_host [--url=<RANCHER_URL> --key=<RANCHER_ACCESS_KEY> --secret=<RANCHER_SECRET_KEY>]"
@@ -16,55 +18,55 @@ def usage(msg = None):
         print "Adds this host to Rancher using the credentials supplied or defined as environment vars"
 
 # get credentials form args or env
-rancher_url = os.environ.get("RANCHER_URL",None)
-rancher_key = os.environ.get("RANCHER_ACCESS_KEY",None)
-rancher_secret = os.environ.get("RANCHER_SECRET_KEY",None)
+rancher_url = os.environ.get("RANCHER_URL", None)
+rancher_key = os.environ.get("RANCHER_ACCESS_KEY", None)
+rancher_secret = os.environ.get("RANCHER_SECRET_KEY", None)
 
-opts, args = getopt.getopt(sys.argv[1:],"hu:k:s:",["help","url=","key=","secret="])
-for o,a in opts:
-        if o in ("-h","--help"):
+opts, args = getopt.getopt(sys.argv[1:], "hu:k:s:", ["help", "url=", "key=", "secret="])
+for o, a in opts:
+        if o in ("-h", "--help"):
                 usage()
                 sys.exit(1)
-        elif o in ("-u","--url"):
+        elif o in ("-u", "--url"):
                 rancher_url = a
-        elif o in ("-k","--key"):
+        elif o in ("-k", "--key"):
                 rancher_key = a
-        elif o in ("-s","--secret"):
+        elif o in ("-s", "--secret"):
                 rancher_secret = a
 
-if rancher_url == None:
+if rancher_url is None:
         usage("Rancher URL not specified")
         sys.exit(1)
-if rancher_key == None:
+if rancher_key is None:
         usage("Rancher key not specified")
         sys.exit(1)
-if rancher_secret == None:
+if rancher_secret is None:
         usage("Rancher secret not specified")
         sys.exit(1)
 
 # split url to protocol and host
-rancher_protocol,rancher_host = rancher_url.split("://")
+rancher_protocol, rancher_host = rancher_url.split("://")
 
 # get environment we're in
-url = "%s://%s:%s@%s/v1/projects" % (rancher_protocol,rancher_key,rancher_secret,rancher_host)
-response = requests.get(url)
-data = json.loads(response.text)
+base_url = "%s://%s:%s@%s/v1/" % (rancher_protocol, rancher_key, rancher_secret, rancher_host)
+response = requests.get(base_url + "projects")
+data = response.json()
 rancher_environment = data['data'][0]['name']
 print "rancher_environment is %s" % rancher_environment
 
 # now ask for a new registration key and wait until it becomes active
-url = "%s://%s:%s@%s/v1/registrationtokens" % (rancher_protocol,rancher_key,rancher_secret,rancher_host)
-response = requests.post(url,json={})
+response = requests.post(base_url + "registrationtokens", json={})
+data = response.json()
 key_active = False
 while not key_active:
-        url = "%s://%s:%s@%s/v1/registrationtokens/%s" % (rancher_protocol,rancher_key,rancher_secret,rancher_host,response.json()['id'])
+        url = base_url + "registrationtokens/" + data['id']
         print url
-        if response.json()['state'] == 'active':
+        if data['state'] == 'active':
                 key_active = True
-                command = response.json()['command']
+                command = data['command']
         else:
                 sleep(0.1)
-                response = requests.get(url)
+                data = requests.get(url).json()
 
 index = command.find('--privileged')
 output_command = command[:index] + ' -e CATTLE_AGENT_IP=`ifconfig eth0 | grep "inet addr" | cut -d \':\' -f 2 | cut -d \' \' -f 1` ' + command[index:]
